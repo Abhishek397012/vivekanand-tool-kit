c85b38177fc77f818c6db9c065a13608e21eb2c4
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2022-05-31 13:51:59 +0530

    added bg to heading and alligned for dekstop view

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { Typography } from "@mui/material"
 
 export default function Heading() {
-    return <Typography variant='h3' style={{ fontWeight: 600 }}>Vivekanand Tool Kit</Typography>
+    return <Typography variant='h3' style={{ fontFamily: "Geneva", backgroundColor: "#005ED6", color: "#ffffff" }}>Vivekanand Tool Kit</Typography>
 }
